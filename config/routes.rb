6561b237e4b676ccda1fbae3789b4cb0b6f04229
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2018-10-16 20:00:27 -0400

    Books, Sales, Authors controller + views generated & added styleguide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  get 'authors/index'
+
+  resources :books, only: [:index, :show]
+  resources :sales, only: [:index, :show, :new]
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
