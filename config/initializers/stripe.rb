63f1ca5f439f211ffa89c76bdc18e356c62261bb
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2018-10-22 10:31:39 -0400

    application name added to stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 require "stripe"
 
 # Stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-# Stripe.api_key = Amyzon::Application.credentials.stripe[:secret_key]
+Stripe.api_key = Bookstore::Application.credentials.stripe[:secret_key]
 
 # any kind of stripe authentication and global initializers goes here
