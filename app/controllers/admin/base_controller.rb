6b77250d79bced7b8d21dbabdd678331c182d29e
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2018-10-21 22:03:43 -0400

    Added gem stripe

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,7 @@ class Admin::BaseController < ApplicationController
   before_filter :ensure_is_admin!
 
   def ensure_is_admin!
-    unless current_user && current_user.admin?
+    unless current_user and current_user.admin?
       flash[:alert] = "You do not have authorization for that"
       redirect_to :back
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def current_user
-    @current_user ||= User.where(id: session[:user_id]).first
-  end
+  # def current_user
+  #   @current_user ||= User.where(id: session[:user_id]).first
+  # end
 end
